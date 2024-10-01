0fe93150d4a0654163513fc15cbc35c9fe023e2f
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2024-10-01 15:47:16 +0300

    hw1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -32,16 +32,4 @@ const Profile = (props) => {
   );
 };
 
-// Profile.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   image: PropTypes.string.isRequired,
-//   stats: PropTypes.shape({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
-
 export default Profile
